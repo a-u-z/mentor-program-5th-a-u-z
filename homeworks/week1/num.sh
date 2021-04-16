9256dea516d580b4dd18e061f90ecc4e8af3e9d4
a-u-z <qinemperor0206@gmail.com>
2021-04-16 14:25:53 +0800

    again

--- homeworks/week1/num.sh
+++ homeworks/week1/num.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-
-
-
-for (( i=1; i<=${1}; i++ ))
-do
-	touch "${i}.js";
-done
-echo "檔案建立完成"
-
+if [ "${1}" == "" ]; then
+	echo "請輸入參數"
+elif [ "${1}" -ge 1 ]; then
+	for (( i=1; i<=${1}; i++ ))
+	do
+		touch "${i}.js";
+	done
+		echo "檔案建立完成"
+else 
+	echo '參數有誤'
+fi
 # http://linux.vbird.org/linux_basic/0340bashshell-scripts.php#for
