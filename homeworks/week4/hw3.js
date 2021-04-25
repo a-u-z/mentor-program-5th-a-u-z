dad98f39e2f42711e2e5acc85112110439771e39
a-u-z <qinemperor0206@gmail.com>
2021-04-25 19:36:50 +0800

    hw3

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -12,7 +12,7 @@ request(
           const { currencies } = json[i].currencies
           console.log(`國家：${json[i].name}`)
           console.log(`首都：${json[i].capital}`)
-          console.log(`貨幣：${currencies.name}`)
+          console.log(`貨幣：${currencies[0].code}`)
           console.log(`國碼：${json[i].callingCodes}`)
         }
       }
