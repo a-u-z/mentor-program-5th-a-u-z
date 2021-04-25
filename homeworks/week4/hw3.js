6f73e3da133dbed19ed59fd6373bb7918e78c529
a-u-z <qinemperor0206@gmail.com>
2021-04-25 18:44:27 +0800

    hw3

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -5,30 +5,20 @@ const process = require('process')
 request(
   `https://restcountries.eu/rest/v2/name/${process.argv[2]}`,
   (error, response, body) => {
-    let json = ''
-    json = body
-    console.log(json)
-    let country = ''
-    let capital = ''
-    let coin = ''
-    let callingCodes = ''
-    const c = json.search('name')
-    const c2 = json.search('topLevelDomain')
-    country = json.slice(c + 7, c2 - 3)
-    const c3 = json.search('capital')
-    const c4 = json.search('altSpellings')
-    capital = json.slice(c3 + 10, c4 - 3)
-    const c5 = json.search('currencies')
-    coin = json.slice(c5 + 22, c5 + 25)
-    const c6 = json.search('callingCodes')
-    if (c6 + 19 === Number) {
-      callingCodes = json.slice(c6 + 16, c6 + 19)
-    } else {
-      callingCodes = json.slice(c6 + 16, c6 + 18)
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      const json = JSON.parse(body)
+      if (json.length > 1) {
+        for (let i = 0; i < json.length; i++) {
+          const { currencies } = json[i].currencies
+          console.log(`國家：${json[i].name}`)
+          console.log(`首都：${json[i].capital}`)
+          console.log(`貨幣：${currencies}`)
+          console.log(`國碼：${json[i].callingCodes}`)
+        }
+      }
     }
-    console.log(`國家：${country}`)
-    console.log(`首都：${capital}`)
-    console.log(`貨幣：${coin}`)
-    console.log(`國碼：${callingCodes}`)
+    if (response.statusCode >= 400 && response.statusCode < 500) {
+      console.log('參數有誤')
+    }
   }
 )
