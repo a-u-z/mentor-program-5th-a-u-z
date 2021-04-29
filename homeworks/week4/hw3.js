a9e6f58aa1853a70a389b59b96218139625222f8
a-u-z <qinemperor0206@gmail.com>
2021-04-29 11:24:39 +0800

    good

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -1,4 +1,4 @@
-function cool() { // 這個 cool function 是為了讓 下面這個 if 可以 return ，來躲過 eslint
+function getCountryInfo() { // 這個 getCountryInfo function 是為了讓 下面這個 if 可以 return ，來躲過 eslint
   const request = require('request')
 
   const process = require('process')
@@ -12,20 +12,18 @@ function cool() { // 這個 cool function 是為了讓 下面這個 if 可以 re
         const json = JSON.parse(body)
         if (json.length > 1) {
           for (let i = 0; i < json.length; i++) {
-            // eslint-disable-next-line
-            const currencies = json[i].currencies
             console.log('============')
             console.log(`國家：${json[i].name}`)
             console.log(`首都：${json[i].capital}`)
-            console.log(`貨幣：${currencies[0].code}`)
+            console.log(`貨幣：${json[i].currencies[0].code}`)
             console.log(`國碼：${json[i].callingCodes}`)
           }
         }
       }
       if (response.statusCode >= 400 && response.statusCode < 500) {
-        console.log('參數有誤')
+        console.log('找不到國家資訊')
       }
     }
   )
 }
-cool()
+getCountryInfo()
