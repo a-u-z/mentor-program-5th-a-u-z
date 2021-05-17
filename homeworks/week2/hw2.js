f2d27de31274e4f9f3553cb8539d3115c9dae128
a-u-z <qinemperor0206@gmail.com>
2021-05-17 20:46:23 +0800

    good

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -11,4 +11,12 @@ function capitalize(str) {
     return (str)
   }
 }
-console.log(capitalize(',hello'))
+console.log(capitalize('hello'))
+function regexCapital(str) {
+  let answer = '' // 宣告答案區
+  const isCapital = /[a-z]/.test(str) // 設定一個判斷傳入字串的第一個字元是否小寫
+  isCapital ? answer += str[0].toUpperCase() : answer += str[0] // 三元判斷式，如果 isCapital 是 true ，那麼 answer 的第一個字母需要轉成大寫，如果是 false 就不用
+  answer += str.slice(1) // 將剩餘的字母放入 answer
+  return answer
+}
+console.log(regexCapital('hello'))
